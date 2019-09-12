5d7094df47eede13277150fc8ef0ae65a31a1c84
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-09-11 17:57:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Changing and deleting, going to make some sort of game
         </p>
         <a
           className="App-link"
