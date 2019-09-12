3f57c4661f67e6d931d641a4969c2d0c6acfe8a8
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-09-11 18:11:50 -0700

    initial ball has been rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Game from './Game';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <p>We still here?</p>
+        <Game />
       </div>
     );
   }
